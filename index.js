c83d568a48d60488624843f7ff444fc7fa4fd68a
AyaanMahimwala <ayaanhaider@gmail.com>
2022-11-16 15:36:42 -0600

    express server running on localhost 3000

---
+++ index.js
@@ -0,0 +1,16 @@
+// Add Express
+const express = require("express");
+
+// Initialize Express
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Create GET request
+app.get("/", (req, res) => {
+  res.send("Express on Vercel");
+});
+
+// Initialize server
+app.listen(port, () => {
+  console.log(`Running on port ${port}.`);
+});
