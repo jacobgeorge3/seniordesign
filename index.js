ddd0e936bc140f7c1992622d17a658cc5438a0ba
jacobgeorge3 <jacob.c.george333@gmail.com>
2022-11-17 11:17:09 -0600

    Revert "update gitignore and get friends from mongo"
    This reverts commit aa3ba7bf16ae2d59378a207287b8403462f10589.

--- index.js
+++ index.js
@@ -1,14 +1,5 @@
 // Add Express
 const express = require("express");
-const { MongoClient } = require("mongodb");
-const connectionString =
-  "mongodb+srv://ut-circles-user:ut-circles-user@cluster0.csb8sik.mongodb.net/?retryWrites=true&w=majority";
-const client = new MongoClient(connectionString, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
-let dbConnection;
 
 // Initialize Express
 const app = express();
@@ -19,30 +10,13 @@ app.get("/", (req, res) => {
   res.send("Testing vercel autoupdate...");
 });
 
-app.get("/get/friends/:username", async (req, res) => {
-  users = await dbConnection.collection("users");
-  //console.log(users);
-  obj = await users.findOne({ username: req.params.username });
-  //console.log(obj);
-  if (obj) {
-    res.send({ friends: obj.friends });
-  } else {
-    res.send({});
-  }
+app.get("/get/friends/:username", (req, res) => {
+  res.send(req.params);
 });
-
-async function connect() {
-  await client.connect();
-  await client.db("Database").command({ ping: 1 });
-  dbConnection = await client.db("Database");
-  //console.log(dbConnection);
-  console.log("Connected successfully to server");
-}
 
 // Initialize server
 app.listen(port, () => {
   console.log(`Running on port ${port}.`);
-  connect();
 });
 
 // Export the Express API for vercel
