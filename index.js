aa3ba7bf16ae2d59378a207287b8403462f10589
AyaanMahimwala <ayaanhaider@gmail.com>
2022-11-16 18:09:01 -0600

    update gitignore and get friends from mongo

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 // Add Express
 const express = require("express");
+const { MongoClient } = require("mongodb");
+const connectionString =
+  "mongodb+srv://ut-circles-user:ut-circles-user@cluster0.csb8sik.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(connectionString, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+let dbConnection;
 
 // Initialize Express
 const app = express();
@@ -10,13 +19,30 @@ app.get("/", (req, res) => {
   res.send("Testing vercel autoupdate...");
 });
 
-app.get("/get/friends/:username", (req, res) => {
-  res.send(req.params);
+app.get("/get/friends/:username", async (req, res) => {
+  users = await dbConnection.collection("users");
+  //console.log(users);
+  obj = await users.findOne({ username: req.params.username });
+  //console.log(obj);
+  if (obj) {
+    res.send({ friends: obj.friends });
+  } else {
+    res.send({});
+  }
 });
+
+async function connect() {
+  await client.connect();
+  await client.db("Database").command({ ping: 1 });
+  dbConnection = await client.db("Database");
+  //console.log(dbConnection);
+  console.log("Connected successfully to server");
+}
 
 // Initialize server
 app.listen(port, () => {
   console.log(`Running on port ${port}.`);
+  connect();
 });
 
 // Export the Express API for vercel
